500236916097a10cb01ca748d746df745700cbb9
Jefferson-Marcelo <57568280+Jefferson-Marcelo@users.noreply.github.com>
2021-05-10 21:57:32 -0300

    Tabelas Atualizadas
    atualização de scripts

--- Inserts.sql
+++ Inserts.sql
@@ -1,3 +1,12 @@
+INSERT INTO funcionario (nome, funcao, salario) VALUES 
+	('Joao Jorge', 'Atendente',1200), 
+	('Jorge Pimentel', 'Atendente',1200),
+	('Guaraci Benito', 'Servico Gerais',1200),
+	('Flaviano Braga', 'Cozinheiro',1500),
+	('Rayme Carvalho', 'Gerente',3200)
+	
+select * from funcionario
+
 INSERT INTO CLIENTE (nome, rg, cpf, bairro, rua, numero, ponto_de_referencia) VALUES 
 	('Joao Barbosa', 12345,'12345678910', 'Mandacaru', 'Beco Ze Borges', 50, 'Bar Ze Pinguco'), 
 	('Jose Franca', 12346,'12345678911', 'Cruz das Armas', 'Rua do Rio', 100, 'Padaria dos Sonhos'), 
@@ -6,6 +15,21 @@ INSERT INTO CLIENTE (nome, rg, cpf, bairro, rua, numero, ponto_de_referencia) VA
 	('Mohamed Alab', 12349,'12345678914', 'Geisel', 'Rua do Estrondo', 50, 'Central de Policia');
 	
 select * from cliente
+
+INSERT INTO telefone (fone, fk_cliente_codcliente, tipo) VALUES (83988887777,1,'Celular');
+INSERT INTO telefone (fone, fk_cliente_codcliente, tipo) VALUES (83988887766,2,'Celular');
+INSERT INTO telefone (fone, fk_cliente_codcliente, tipo) VALUES (83988887755,3,'Celular');
+INSERT INTO telefone (fone, fk_cliente_codcliente, tipo) VALUES (83988887744,4,'Celular');
+INSERT INTO telefone (fone, fk_cliente_codcliente, tipo) VALUES (83988887733,5,'Celular');
+
+select * from telefone;
+
+INSERT INTO motoboy (nome) VALUES ('Jefferson')
+INSERT INTO motoboy (nome) VALUES ('Cachorro_Loko')
+INSERT INTO motoboy (nome) VALUES ('Moleque_du_Grau')
+
+select * from motoboy;
+
 
 INSERT INTO Categoria (categoria) VALUES 
 	('Pizza'), 
@@ -16,12 +40,117 @@ INSERT INTO Categoria (categoria) VALUES
 	
 select * from categoria
 
-INSERT INTO funcionario (nome, funcao, salario) VALUES 
-	('Joao Jorge', 'Atendente',1200), 
-	('Jorge Pimentel', 'Atendente',1200),
-	('Guaraci Benito', 'Servico Gerais',1200),
-	('Flaviano Braga', 'Cozinheiro',1500),
-	('Rayme Carvalho', 'Gerente',3200)
-	
-select * from funcionario
+
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Mista Pequena');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Mista Média');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Mista Grande');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Mista Familia');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Frango com Catupiry Pequena');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Frango com Catupiry Média');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Frango com Catupiry Grande');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Frango com Catupiry Familia');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Calabresa Pequena');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Calabresa Média');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Calabresa Grande');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Calabresa Familia');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Charque Pequena');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Charque Média');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Charque Grande');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza Charque Familia');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza A Moda Pequena');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza A Moda Média');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza A Moda Grande');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (1,5, 'Pizza A Moda Familia');
+
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (2,5, 'Coxinha');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (2,5, 'Enroladinho');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (2,5, 'Empada');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (2,5, 'Pastel de Frango');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (2,5, 'Pastel de Carne');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (2,5, 'Pastel Misto');
+
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (3,5, 'Acai no copo');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (3,5, 'Acai Tigela P');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (3,5, 'Acai Tigela M');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (3,5, 'Acai Tigela G');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (3,5, 'Barca Acai');
+
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (4,5, 'Torta de chocolate');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (4,5, 'Bolo no pote');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (4,5, 'Pudim');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (4,5, 'Sorvete sabores');
+
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (5,5, 'Misto quente');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (5,5, 'Hamburguer tradicional');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (5,5, 'X-Frango');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (5,5, 'X-Calabresa');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (5,5, 'X-Egg');
+INSERT INTO produto (fk_categoria_codcategoria_pk, estoque, nome) VALUES (5,5, 'X-Tudo');
+
+update produto set preco = 20.0 where codproduto = 1;
+update produto set preco = 25.0 where codproduto = 2;
+update produto set preco = 35.0 where codproduto = 3;
+update produto set preco = 40.0 where codproduto = 4;
+update produto set preco = 20.0 where codproduto = 5;
+update produto set preco = 25.0 where codproduto = 6;
+update produto set preco = 35.0 where codproduto = 7;
+update produto set preco = 40.0 where codproduto = 8;
+update produto set preco = 20.0 where codproduto = 9;
+update produto set preco = 25.0 where codproduto = 10;
+update produto set preco = 35.0 where codproduto = 11;
+update produto set preco = 40.0 where codproduto = 12;
+update produto set preco = 20.0 where codproduto = 13;
+update produto set preco = 25.0 where codproduto = 14;
+update produto set preco = 35.0 where codproduto = 15;
+update produto set preco = 40.0 where codproduto = 16;
+update produto set preco = 20.0 where codproduto = 17;
+update produto set preco = 25.0 where codproduto = 18;
+update produto set preco = 35.0 where codproduto = 19;
+update produto set preco = 40.0 where codproduto = 20;
+update produto set preco = 3.5 where codproduto = 21;
+update produto set preco = 3.5 where codproduto = 22;
+update produto set preco = 3.5 where codproduto = 23;
+update produto set preco = 4.0 where codproduto = 24;
+update produto set preco = 3.5 where codproduto = 25;
+update produto set preco = 4.5 where codproduto = 26;
+update produto set preco = 3.0 where codproduto = 27;
+update produto set preco = 5.0 where codproduto = 28;
+update produto set preco = 10.0 where codproduto = 29;
+update produto set preco = 15.0 where codproduto = 30;
+update produto set preco = 30.0 where codproduto = 31;
+update produto set preco = 10.0 where codproduto = 32;
+update produto set preco = 5.0 where codproduto = 33;
+update produto set preco = 7.0 where codproduto = 34;
+update produto set preco = 7.0 where codproduto = 35;
+update produto set preco = 5.0 where codproduto = 36;
+update produto set preco = 5.0 where codproduto = 37;
+update produto set preco = 13.0 where codproduto = 38;
+update produto set preco = 12.0 where codproduto = 39;
+update produto set preco = 14.0 where codproduto = 40;
+
+INSERT INTO 
+pedido (fk_cliente_codcliente, fk_motoboy_codmotoboy, fk_funcionario_codfuncionario, data, status, taxa_entrega) 
+VALUES (1,1,1,current_timestamp, 'Em preparo', 10);
+INSERT INTO 
+pedido (fk_cliente_codcliente, fk_motoboy_codmotoboy, fk_funcionario_codfuncionario, data, status, taxa_entrega) 
+VALUES (2,1,1,current_timestamp, 'Em preparo', 8.00);
+INSERT INTO 
+pedido (fk_cliente_codcliente, fk_motoboy_codmotoboy, fk_funcionario_codfuncionario, data, status, taxa_entrega) 
+VALUES (2,2,2,current_timestamp, 'Em preparo', 8.00);
+INSERT INTO 
+pedido (fk_cliente_codcliente, fk_motoboy_codmotoboy, fk_funcionario_codfuncionario, data, status, taxa_entrega) 
+VALUES (3,2,2,current_timestamp, 'Em preparo', 20.00);
+INSERT INTO 
+pedido (fk_cliente_codcliente, fk_motoboy_codmotoboy, fk_funcionario_codfuncionario, data, status, taxa_entrega) 
+VALUES (5,2,1,current_timestamp, 'Em preparo', 9.00);
+
+
+INSERT INTO preparado (fk_funcionario_codfuncionario, fk_pedido_codpedido) VALUES (1,1);
+INSERT INTO preparado (fk_funcionario_codfuncionario, fk_pedido_codpedido) VALUES (1,2);
+INSERT INTO preparado (fk_funcionario_codfuncionario, fk_pedido_codpedido) VALUES (1,3);
+INSERT INTO preparado (fk_funcionario_codfuncionario, fk_pedido_codpedido) VALUES (1,4);
+INSERT INTO preparado (fk_funcionario_codfuncionario, fk_pedido_codpedido) VALUES (1,5);
+
+
+
 
--- Tabelas.sql
+++ Tabelas.sql
@@ -1,142 +1,124 @@
-CREATE TABLE Cliente (
-    CodCliente INT  PRIMARY KEY,
-    Nome VARCHAR,
-    RG INT,
-    CPF VARCHAR,
-    Bairro VARCHAR,
-    Rua VARCHAR,
-    Numero INT,
-    Ponto_de_referencia VARCHAR,
-    UNIQUE (CodCliente, RG, CPF)
-);
-
-CREATE TABLE Motoboy (
-    CodMotoboy INT PRIMARY KEY,
-    Nome VARCHAR
-);
-
-CREATE TABLE Produto (
-    CodProduto INT PRIMARY KEY,
-    fk_Categoria_CodCategoria_PK INT,
-    Quantidade INT
-	Nome VARCHAR(60)
-);
-
-CREATE TABLE Funcionario (
-    CodFuncionario INT PRIMARY KEY,
-    Nome VARCHAR,
-    Funcao VARCHAR,
-    Salario DECIMAL
-);
-
-CREATE TABLE Pedido (
-    CodPedido INT PRIMARY KEY,
-    fk_Cliente_CodCliente INT,
-    fk_Motoboy_CodMotoboy INT,
-    fk_Funcionario_CodFuncionario INT,
-    Data TIMESTAMP,
-    Staus VARCHAR,
-    Valor DECIMAL,
-    Taxa_Entrega DECIMAL
-);
-
-CREATE TABLE Telefone (
-    Fone INT,
-    fk_Cliente_CodCliente INT NOT NULL UNIQUE,
-    Tipo VARCHAR,
-    PRIMARY KEY (fk_Cliente_CodCliente, Fone)
-);
-
-CREATE TABLE Categoria (
-    CodCategoria_PK INT NOT NULL PRIMARY KEY,
-    Categoria VARCHAR
-);
-
-CREATE TABLE Gerencia (
-    fk_Funcionario_CodFuncionario INT,
-    fk_Funcionario_CodFuncionario_ INT,
-    Valor_Total DECIMAL
-);
-
-CREATE TABLE Preparado (
-    fk_Funcionario_CodFuncionario INT,
-    fk_Pedido_CodPedido INT
-);
-
-CREATE TABLE Compoe (
-    fk_Produto_CodProduto INT,
-    fk_Pedido_CodPedido INT
-);
- 
-ALTER TABLE Produto ADD CONSTRAINT FK_Produto_2
-    FOREIGN KEY (fk_Categoria_CodCategoria_PK)
-    REFERENCES Categoria (CodCategoria_PK)
-    ON DELETE NO ACTION;
- 
-ALTER TABLE Pedido ADD CONSTRAINT FK_Pedido_2
-    FOREIGN KEY (fk_Cliente_CodCliente)
-    REFERENCES Cliente (CodCliente)
-    ON DELETE CASCADE;
- 
-ALTER TABLE Pedido ADD CONSTRAINT FK_Pedido_3
-    FOREIGN KEY (fk_Motoboy_CodMotoboy)
-    REFERENCES Motoboy (CodMotoboy)
-    ON DELETE CASCADE;
- 
-ALTER TABLE Pedido ADD CONSTRAINT FK_Pedido_4
-    FOREIGN KEY (fk_Funcionario_CodFuncionario)
-    REFERENCES Funcionario (CodFuncionario)
-    ON DELETE CASCADE;
-
-select * from telefone
- 
-ALTER TABLE Telefone ADD CONSTRAINT FK_Telefone_2
-    FOREIGN KEY (fk_Cliente_CodCliente)
-    REFERENCES Cliente (codcliente);
- 
-ALTER TABLE Gerencia ADD CONSTRAINT FK_Gerencia_1
-    FOREIGN KEY (fk_Funcionario_CodFuncionario)
-    REFERENCES Funcionario (CodFuncionario)
-    ON DELETE CASCADE;
- 
-ALTER TABLE Gerencia ADD CONSTRAINT FK_Gerencia_2
-    FOREIGN KEY (fk_Funcionario_CodFuncionario_)
-    REFERENCES Funcionario (CodFuncionario)
-    ON DELETE CASCADE;
- 
-ALTER TABLE Preparado ADD CONSTRAINT FK_Preparado_1
-    FOREIGN KEY (fk_Funcionario_CodFuncionario)
-    REFERENCES Funcionario (CodFuncionario)
-    ON DELETE RESTRICT;
- 
-ALTER TABLE Preparado ADD CONSTRAINT FK_Preparado_2
-    FOREIGN KEY (fk_Pedido_CodPedido)
-    REFERENCES Pedido (CodPedido)
-    ON DELETE SET NULL;
- 
-ALTER TABLE Compoe ADD CONSTRAINT FK_Compoe_1
-    FOREIGN KEY (fk_Produto_CodProduto)
-    REFERENCES Produto (CodProduto)
-    ON DELETE RESTRICT;
- 
-ALTER TABLE Compoe ADD CONSTRAINT FK_Compoe_2
-    FOREIGN KEY (fk_Pedido_CodPedido)
-    REFERENCES Pedido (CodPedido)
-    ON DELETE RESTRICT;
-	
-ALTER TABLE compoe
-ADD COLUMN quantidade int
+CREATE TABLE public.funcionario
+(
+    codfuncionario integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    nome character varying COLLATE pg_catalog."default",
+    funcao character varying COLLATE pg_catalog."default",
+    salario numeric,
+    codgerente integer,
+    valortotal numeric,
+    CONSTRAINT funcionario_pkey PRIMARY KEY (codfuncionario),
+    CONSTRAINT salariopositivo CHECK (salario > 0::numeric)
+)
 
 
-ALTER TABLE cliente
-ADD CONSTRAINT tamanhoCPF
-CHECK (length (CPF) = 11);
+CREATE TABLE public.cliente
+(
+    codcliente integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    nome character varying COLLATE pg_catalog."default",
+    rg integer,
+    cpf character varying COLLATE pg_catalog."default",
+    bairro character varying COLLATE pg_catalog."default",
+    rua character varying COLLATE pg_catalog."default",
+    numero integer,
+    ponto_de_referencia character varying COLLATE pg_catalog."default",
+    CONSTRAINT cliente_pkey PRIMARY KEY (codcliente),
+    CONSTRAINT cliente_codcliente_rg_cpf_key UNIQUE (codcliente, rg, cpf),
+    CONSTRAINT tamanhocpf CHECK (length(cpf::text) = 11)
+)
 
-ALTER TABLE funcionario
-ADD CONSTRAINT salariopositivo
-CHECK (salario > 0);
 
-ALTER TABLE PRODUTO
-ALTER COLUMN Estoque
-WHERE COLUMN = 'quantidade'
+CREATE TABLE public.telefone
+(
+    fone character varying COLLATE pg_catalog."default" NOT NULL,
+    fk_cliente_codcliente integer NOT NULL,
+    tipo character varying COLLATE pg_catalog."default",
+    CONSTRAINT telefone_pkey PRIMARY KEY (fk_cliente_codcliente, fone),
+    CONSTRAINT telefone_fk_cliente_codcliente_key UNIQUE (fk_cliente_codcliente),
+    CONSTRAINT fk_telefone_2 FOREIGN KEY (fk_cliente_codcliente)
+        REFERENCES public.cliente (codcliente) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
 
+
+CREATE TABLE public.motoboy
+(
+    codmotoboy integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    nome character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT motoboy_pkey PRIMARY KEY (codmotoboy)
+)
+
+CREATE TABLE public.categoria
+(
+    codcategoria_pk integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    categoria character varying COLLATE pg_catalog."default",
+    CONSTRAINT categoria_pkey PRIMARY KEY (codcategoria_pk)
+)
+
+CREATE TABLE public.produto
+(
+    codproduto integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    fk_categoria_codcategoria_pk integer,
+    estoque integer,
+    nome character varying(60) COLLATE pg_catalog."default",
+    preco numeric,
+    CONSTRAINT produto_pkey PRIMARY KEY (codproduto),
+    CONSTRAINT fk_produto_2 FOREIGN KEY (fk_categoria_codcategoria_pk)
+        REFERENCES public.categoria (codcategoria_pk) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+CREATE TABLE public.pedido
+(
+    codpedido integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    fk_cliente_codcliente integer,
+    fk_motoboy_codmotoboy integer,
+    fk_funcionario_codfuncionario integer,
+    data timestamp without time zone,
+    status character varying COLLATE pg_catalog."default",
+    valor numeric,
+    taxa_entrega numeric,
+    CONSTRAINT pedido_pkey PRIMARY KEY (codpedido),
+    CONSTRAINT fk_pedido_2 FOREIGN KEY (fk_cliente_codcliente)
+        REFERENCES public.cliente (codcliente) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE,
+    CONSTRAINT fk_pedido_3 FOREIGN KEY (fk_motoboy_codmotoboy)
+        REFERENCES public.motoboy (codmotoboy) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE,
+    CONSTRAINT fk_pedido_4 FOREIGN KEY (fk_funcionario_codfuncionario)
+        REFERENCES public.funcionario (codfuncionario) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+)
+
+CREATE TABLE public.compoe
+(
+    fk_produto_codproduto integer NOT NULL,
+    fk_pedido_codpedido integer NOT NULL,
+    quantidade integer NOT NULL,
+    CONSTRAINT fk_compoe_1 FOREIGN KEY (fk_produto_codproduto)
+        REFERENCES public.produto (codproduto) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_compoe_2 FOREIGN KEY (fk_pedido_codpedido)
+        REFERENCES public.pedido (codpedido) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE RESTRICT
+)
+
+CREATE TABLE public.preparado
+(
+    fk_funcionario_codfuncionario integer,
+    fk_pedido_codpedido integer,
+    CONSTRAINT fk_preparado_1 FOREIGN KEY (fk_funcionario_codfuncionario)
+        REFERENCES public.funcionario (codfuncionario) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_preparado_2 FOREIGN KEY (fk_pedido_codpedido)
+        REFERENCES public.pedido (codpedido) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE SET NULL
+)
